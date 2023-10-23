effaa2249f07b429a524b87efc7218de942ff543
vaschenkoff <vaschenkoff@mail.ru>
2023-10-23 15:49:13 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+
+        System.out.println("Andrey Vaschenkov");
+        System.out.println("01.07.1992");
+    }
+}
