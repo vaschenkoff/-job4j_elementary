e858cf70b8d21b40bf5a913948d9c6cd43d9584a
Andrew Vaschenkoff <vaschenkoff@mail.ru>
2023-10-25 17:24:43 +0300

    4.2. Тернарное сравнение [#189]

---
+++ src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class Max {
+    public static int max(int left, int right) {
+        boolean condition = left > right;
+        int result = condition ? left : right;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int rsl = Max.max(4, 4);
+        System.out.println(rsl);
+    }
+}
