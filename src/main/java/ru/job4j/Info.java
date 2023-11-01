ea231f3ca730f550e09ac8038e543234f29ab8d0
Andrew Vaschenkoff <vaschenkoff@mail.ru>
2023-11-01 15:04:45 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/Info.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j;
-
-public class Info {
-    public static void main(String[] args) {
-
-        System.out.println("23.10.2023");
-    }
-}
--- src/main/java/ru/job4j/StudentInfo.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j;
-
-public class StudentInfo {
-    public static void main(String[] args) {
-
-        System.out.println("Andrey Vaschenkov");
-        System.out.println("01.07.1992");
-    }
-}
